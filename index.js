afeb5c2bd57d8b6eda649451ea71c73c70a4e81c
Sacettin Şahin <sacettin.sahin@etiya.com>
2023-09-16 18:14:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const db = require("./config/db.js");
+const AuthRoute = require("./routes/auth.js");
 
 dotenv.config();
 const app = express();
 app.use(cors());
+app.use(express.json({limit:'30mb', extended: true}));
+app.use(express.urlencoded({limit:'30mb', extended: true}));
 
-const PORT = process.env.PORT || 5000;
+app.use("/", AuthRoute);
 
 app.get("/", (req, res) => {
     res.json({ message: "Hello World" });
 });
 
+const PORT = process.env.PORT || 5000;
 db();
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
